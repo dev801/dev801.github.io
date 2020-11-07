f50f2325256e2535b3ebc8fad3d4a66cfbf6558d
George-801 <56756124+George-801@users.noreply.github.com>
2020-11-07 11:49:03 +0000

    e

--- botbiscuit/manual/content.js
+++ botbiscuit/manual/content.js
@@ -1,3 +1,5 @@
+pageDataPrefix = "";
+
 async function loadSidebar() {
     sidebarJSONURL = "https://dev801.github.io/botbiscuit/manual/sidebar.json"
     sidebarJSON = undefined;
@@ -15,4 +17,50 @@ async function loadSidebar() {
 
     sidebarContent = document.getElementById("sidebarContent");
     sidebarContent.innerHTML = "";
+
+    replaceSidebar = "";
+    sidebarContents = sidebarJSON.contents;
+    for (i = 0; i < sidebarContents.length; i++) {
+        if (sidebarContents[i].heading) {
+            replaceSidebar += `<p>${sidebarContents[i].name}</p>`
+            continue;
+        }
+        if (sidebarContents[i].contents == undefined) {
+            replaceSidebar += `<li><a href="${sidebarContents[i].link}">${sidebarContents[i].name}</a></li>`;
+            continue;
+        }
+        dropdownContent = ""
+        for (x = 0; x < sidebarContents[i].contents.length; x++) {
+            dropdownContent += `<li><a href="${sidebarContents[i].contents[x].link}">${sidebarContents[i].contents[x].name}</a></li>`;
+        }
+        replaceSidebar += `<li><a href="#${sidebarContents[i].name.replace(" ", "")}Submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${sidebarContents[i].name}</a><ul class="collapse list-unstyled" id="${sidebarContents[i].name.replace(" ", "")}Submenu">${dropdownContent}</ul></li>`
+    }
+
+    sidebarContent.innerHTML = replaceSidebar;
+}
+
+async function loadPageData() {
+    try {
+        manualJSONURL = "https://dev801.github.io/botbiscuit/manual/manual.json"
+        manualJSON = undefined;
+
+        select = document.getElementById("langSelect");
+        language = select.options[select.selectedIndex].value
+        if (language == undefined) {
+            language = "English"
+        }
+        await $.getJSON(manualJSONURL, function(json) {
+            pageDataPrefix = json["prefix"]
+            manualJSON = json[language][getManualPath()]
+        });
+
+        document.getElementById("pgTitle").innerHTML = manualJSON.title;
+        document.getElementById("pgContent").innerHTML = manualJSON.title;
+    } catch (e) {
+        document.getElementById("pgContent").innerHTML = `Something went horribly wrong with loading the page content - this is most likely because i messed up a JSON file somewhere.<br><kbd>${e.stack}</kbd>`
+    }
+}
+
+function getManualPath() {
+    return window.location.href.slice(pageDataPrefix.length);
 }
